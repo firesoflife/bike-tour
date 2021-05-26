5053752af0a5d126d48c6ec1cf7717afd80ab7aa
firesoflife <bryangoertz@gmail.com>
2021-05-25 22:07:53 -0400

    first commit

---
+++ app/javascript/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Home from './Home';
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+};
+
+export default App;
---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className='section-body'>
+      <div className='background'>
+        <div className='title-background'></div>
+        <div className='title-content'>
+          <h1>
+            Boulder
+            <span className='cross-text'> Cyclocross </span> Championhsip
+          </h1>
+          <h2 className='sub-title'>Bikes for All</h2>
+        </div>
+        <div className='down-arrow'>
+          <i className='fas fa-arrow-down'></i>
+        </div>
+      </div>
+      <div className='section-1'>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci
+          voluptatem placeat minus sed doloremque culpa nesciunt ut, ad sequi
+          eum explicabo enim quae voluptas eveniet minima consequatur ex, magnam
+          necessitatibus. Neque quia possimus voluptatibus praesentium aut a
+          officia pariatur sequi sint nesciunt non, cumque nulla modi quod saepe
+          eos. Nostrum eos corporis maiores ipsam quas pariatur dolor distinctio
+          molestias delectus sed dicta tenetur, necessitatibus sint accusamus
+          hic. Molestias expedita, quia laboriosam quas dignissimos asperiores
+          recusandae facilis non soluta consectetur! Et veritatis aperiam vero
+          rerum quasi deleniti unde nulla ipsam cum quos dolore labore itaque
+          atque explicabo qui, cupiditate dolor facere architecto officiis
+          corrupti, consequatur minus at natus magni. Quos dolore ratione nulla,
+          eum quas velit minus commodi. Numquam, pariatur? Fuga enim, autem
+          molestias maxime iure, nam suscipit minima deserunt sequi cum dolore
+          fugit hic atque harum ipsam eius. Enim quam corrupti illum ratione
+          dignissimos molestiae tempora inventore, nesciunt officia, incidunt
+          esse quod placeat? Enim pariatur culpa dolorem sint repellat fuga
+          error, nesciunt maiores quisquam explicabo assumenda accusamus
+          deserunt hic eveniet vero? Cupiditate repellendus numquam aut impedit
+          nihil voluptate alias, quibusdam ipsum, ipsam debitis ex ab sed! Et
+          repellat saepe, magnam doloribus autem quo veritatis! Ut temporibus
+          corrupti eius quasi. Veritatis incidunt obcaecati, pariatur autem
+          quaerat quod. Minus, ullam voluptate ut totam reiciendis facere
+          accusantium quas harum magni, hic consequuntur labore cupiditate
+          voluptas distinctio explicabo consectetur nemo facilis autem
+          voluptatibus! Saepe, cum voluptatum minus debitis nisi corporis
+          facilis nam ut rem fugit minima mollitia libero expedita magnam iste
+          laborum perferendis ex a consequuntur suscipit inventore, quis itaque.
+          Aliquam laudantium iste eligendi ut facilis? Doloribus eos ratione,
+          iusto similique culpa repellat mollitia nesciunt sint dolor libero
+          recusandae commodi repellendus, quo blanditiis excepturi voluptates a
+          earum repudiandae voluptatum cupiditate inventore adipisci! Quae vel
+          dolorum delectus doloremque beatae, odit quibusdam perspiciatis, quod
+          dolor excepturi est ad recusandae suscipit quos similique doloribus
+          error, odio eaque laboriosam labore minus. Hic iure inventore, soluta
+          quisquam quia corporis veniam, blanditiis consequatur quos illo culpa
+          quis quam aliquid suscipit tempore at voluptatem ipsum, laboriosam
+          ipsa. Ab explicabo ad architecto quisquam suscipit ex facere? Minima
+          quod, nesciunt ipsam maiores voluptate laudantium quasi quibusdam
+          perspiciatis earum deserunt labore suscipit, magni facilis corporis
+          provident, culpa placeat? Sint sequi asperiores ipsam consectetur odit
+          autem natus eos molestias omnis ullam neque doloremque quisquam
+          incidunt minima exercitationem nulla suscipit ipsa praesentium
+          molestiae, nesciunt quaerat alias voluptas aperiam? Vel amet culpa,
+          facilis dolores veniam ut, totam modi, aut accusantium tempore eum
+          quasi aperiam commodi. Quia, enim totam. Nobis doloremque quidem,
+          natus nesciunt quibusdam inventore quas praesentium pariatur.
+          Temporibus, hic eius sed, fugit possimus aut a ratione deleniti animi
+          totam ut ad quos repellat dolore quia reprehenderit deserunt esse
+          expedita inventore assumenda voluptatum veritatis unde consequatur! Ex
+          perferendis ut dolores rerum eaque. Similique, molestiae possimus eius
+          sapiente facere id amet. Eius asperiores cumque fugit quis sunt dolore
+          enim atque ad numquam itaque ut esse ducimus repellat quas quia
+          consequatur, et est cupiditate hic blanditiis animi in distinctio
+          laudantium. Unde delectus, eveniet commodi aut nobis et inventore
+          ducimus.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
